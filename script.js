112fd15122dd749bbe44ce0fdbffadd472ce5379
Sayed Shah <133984128+sshahmuneer@users.noreply.github.com>
2023-07-04 15:51:19 -0400

    Create operate function that calls one of the arithmetic operations on two numbers

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, firstNumber, secondNumber) {
+    switch(operator) {
+        case '+':
+            add(firstNumber, secondNumber);
+            break;
+        case '-':
+            subtract(firstNumber, secondNumber);
+            break;
+        case '*':
+            multiply(firstNumber, secondNumber);
+            break;
+        case '/':
+            divide(firstNumber, secondNumber);
+            break;
+        default:
+    }
+}
